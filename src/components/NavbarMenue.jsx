f62ff6b6f3c26ae415afd2528946cd86bbeff20a
Khakijanov <haqijonov@gmail.com>
2024-07-05 20:10:43 +0500

    yaxshilandi

--- src/components/NavbarMenue.jsx
+++ src/components/NavbarMenue.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 
 function NavbarMenue() {
   return (
-    <div className='container max-w-[910px] mx-auto px-14 py-6'>
+    <div className='container max-w-[910px] mx-auto px-14 py-6 hidden sm:block'>
         <ul className='flex justify-between items-center '>
             <li className='relative myAfterRight  px-10 text-gray-500 hover:text-PrimaryColor transitionAll font-semibold hover: '><a href="#">Home</a></li>
             <li className='relative myAfterRight myAfterLeft px-10 text-gray-500 hover:text-PrimaryColor transitionAll font-semibold'><a href="#">About</a></li>
